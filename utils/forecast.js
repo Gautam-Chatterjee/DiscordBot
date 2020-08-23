02d59e99cab77720cb89f74389027bbd42770478
Gautam-Chatterjee <gchatterjee@umass.edu>
2020-08-23 13:02:27 +0530

    new key

--- utils/forecast.js
+++ utils/forecast.js
@@ -2,7 +2,7 @@ const request = require('request')
 
 const forecast = (latitude,longitude,callback) =>{
      
-    const url = 'http://api.weatherstack.com/current?access_key=af86cae01490718f0d48e6b4d66ed64a&query='+ latitude+','+ longitude +'&units=f'
+    const url = 'http://api.weatherstack.com/current?access_key=7711ee9ecd462c7b8d7b0fb8d0ed3e9f&query='+ latitude+','+ longitude +'&units=f'
 
     request({url, json: true}, (error,{body}) => {
       if(error){
